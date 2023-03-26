aab6bb8faff51e2ed0d9c40ccb9e881dc40aefd6
marquesdavi <davibaterabatera@hotmail.com>
2023-03-26 00:51:45 -0300

    feature: Created Student and Course database models

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'rg', 'cpf', 'birth_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'course_code', 'description')
+    list_display_links = ('id', 'course_code')
+    search_fields = ('course_code',)
+
+admin.site.register(Course, Courses)
